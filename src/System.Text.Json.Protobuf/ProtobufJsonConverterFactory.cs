ba834473f86bfbda1e82e742d809979f6dead4a6
Havret <h4vret@gmail.com>
2022-03-19 14:32:55 +0100

    Use property naming policy

--- src/System.Text.Json.Protobuf/ProtobufJsonConverterFactory.cs
+++ src/System.Text.Json.Protobuf/ProtobufJsonConverterFactory.cs
@@ -5,6 +5,13 @@ namespace System.Text.Json.Protobuf;
 
 internal class ProtobufJsonConverterFactory : JsonConverterFactory
 {
+    private readonly JsonProtobufSerializerOptions _options;
+
+    public ProtobufJsonConverterFactory(JsonProtobufSerializerOptions options)
+    {
+        _options = options;
+    }
+
     public override bool CanConvert(Type typeToConvert)
     {
         return typeToConvert.IsAssignableTo(typeof(IMessage));
@@ -13,6 +20,6 @@ internal class ProtobufJsonConverterFactory : JsonConverterFactory
     public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
         var converterType = typeof(ProtobufConverter<>).MakeGenericType(typeToConvert);
-        return (JsonConverter) Activator.CreateInstance(converterType)!;
+        return (JsonConverter) Activator.CreateInstance(converterType, new object[]{options.PropertyNamingPolicy, _options})!;
     }
 }
