590f2e2ca5a3fbf65ce148575f79765d446eface
Havret <h4vret@gmail.com>
2022-06-23 15:52:50 +0200

    Fix #51

--- test/Protobuf.System.Text.Json.Tests/SchemaEvolutionTests.cs
+++ test/Protobuf.System.Text.Json.Tests/SchemaEvolutionTests.cs
@@ -1,5 +1,10 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Protobuf.Tests;
+using System.Threading.Tasks;
 using Protobuf.System.Text.Json.Tests.Utils;
 using Shouldly;
 using Xunit;
@@ -28,5 +33,28 @@ public class SchemaEvolutionTests
 
         // Assert
         deserialized.ShouldHaveSingleItem();
+    }
+    
+    [Fact]
+    public async Task Should_be_able_to_deserialize_array_of_schema_v2_to_array_of_schema_v1_when_schema_v2_has_additional_repeated_field_2()
+    {
+        // Arrange
+        var schemaEvolutionV2 = Enumerable.Range(0, 50_000).Select(x => new SchemaEvolutionV2
+        {
+            Property1 = x,
+            Property2 = {x + 1, x + 2}
+        }).ToList();
+
+        var jsonSerializerOptions = TestHelper.CreateJsonSerializerOptions();
+        using var memoryStream = new MemoryStream();
+        await JsonSerializer.SerializeAsync(memoryStream, schemaEvolutionV2, jsonSerializerOptions);
+        memoryStream.Position = 0;
+
+        // Act
+        var deserialized = JsonSerializer.Deserialize<SchemaEvolutionV1[]>(memoryStream, jsonSerializerOptions);
+
+        // Assert
+        deserialized.ShouldNotBeNull();
+        deserialized.Length.ShouldBe(schemaEvolutionV2.Count);
     }
 }
