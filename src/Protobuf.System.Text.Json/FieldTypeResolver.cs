685c523496dfa7d156d129f95c8fa62047729112
Havret <h4vret@gmail.com>
2022-06-23 20:38:56 +0200

    Move field type resolving logic to separate class

---
+++ src/Protobuf.System.Text.Json/FieldTypeResolver.cs
@@ -0,0 +1,64 @@
+using Google.Protobuf.Reflection;
+using Google.Protobuf.WellKnownTypes;
+using Type = System.Type;
+
+namespace Protobuf.System.Text.Json;
+
+internal static class FieldTypeResolver
+{
+    public static Type ResolverFieldType(FieldDescriptor fieldDescriptor, Dictionary<string, Type> propertyTypeLookup)
+    {
+        switch (fieldDescriptor.FieldType)
+        {
+            case FieldType.Double:
+                return typeof(double);
+            case FieldType.Float:
+                return typeof(float);
+            case FieldType.Int32:
+            case FieldType.SInt32:
+            case FieldType.SFixed32:
+                return typeof(int);
+            case FieldType.Int64:
+            case FieldType.SInt64:
+            case FieldType.SFixed64:
+                return typeof(long);
+            case FieldType.UInt32:
+            case FieldType.Fixed32:
+                return typeof(uint);
+            case FieldType.UInt64:
+            case FieldType.Fixed64:
+                return typeof(ulong);
+            case FieldType.Bool:
+                return typeof(bool);
+            case FieldType.String:
+                return typeof(string);
+            case FieldType.Message when fieldDescriptor.MessageType.ClrType is { } clrType:
+                if (clrType == typeof(DoubleValue))
+                    return typeof(double?);
+                if (clrType == typeof(FloatValue))
+                    return typeof(float?);
+                if (clrType == typeof(Int64Value))
+                    return typeof(long?);
+                if (clrType == typeof(UInt64Value))
+                    return typeof(ulong?);
+                if (clrType == typeof(Int32Value))
+                    return typeof(int?);
+                if (clrType == typeof(UInt32Value))
+                    return typeof(uint?);
+                if (clrType == typeof(BoolValue))
+                    return typeof(bool?);
+                if (clrType == typeof(StringValue))
+                    return typeof(string);
+                return clrType;
+            case FieldType.Enum when fieldDescriptor.IsRepeated:
+                var fieldType = propertyTypeLookup[fieldDescriptor.PropertyName];
+                return fieldType.GenericTypeArguments[0];
+            case FieldType.Enum:
+            case FieldType.Message:
+                return propertyTypeLookup[fieldDescriptor.PropertyName];
+            default:
+                throw new ArgumentOutOfRangeException(nameof(fieldDescriptor),
+                    $"FieldType: '{fieldDescriptor}' is not supported.");
+        }
+    }
+}
--- src/Protobuf.System.Text.Json/ProtobufConverter.cs
+++ src/Protobuf.System.Text.Json/ProtobufConverter.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Google.Protobuf;
 using Google.Protobuf.Reflection;
-using Google.Protobuf.WellKnownTypes;
 using Protobuf.System.Text.Json.InternalConverters;
 using Type = System.Type;
 
@@ -33,7 +32,7 @@ internal class ProtobufConverter<T> : JsonConverter<T?> where T : class, IMessag
             Accessor = fieldDescriptor.Accessor,
             IsRepeated = fieldDescriptor.IsRepeated,
             IsMap = fieldDescriptor.IsMap,
-            FieldType = GetFieldType(fieldDescriptor, propertyTypeLookup),
+            FieldType = FieldTypeResolver.ResolverFieldType(fieldDescriptor, propertyTypeLookup),
             JsonName = convertNameFunc(fieldDescriptor),
             IsOneOf = fieldDescriptor.ContainingOneof != null
         }).ToArray();
@@ -141,60 +140,5 @@ internal class ProtobufConverter<T> : JsonConverter<T?> where T : class, IMessag
         }
 
         writer.WriteEndObject();
-    }
-
-    private Type GetFieldType(FieldDescriptor fieldDescriptor, Dictionary<string, Type> propertyTypeLookup)
-    {
-        switch (fieldDescriptor.FieldType)
-        {
-            case FieldType.Double:
-                return typeof(double);
-            case FieldType.Float:
-                return typeof(float);
-            case FieldType.Int32:
-            case FieldType.SInt32:
-            case FieldType.SFixed32:
-                return typeof(int);
-            case FieldType.Int64:
-            case FieldType.SInt64:
-            case FieldType.SFixed64:
-                return typeof(long);
-            case FieldType.UInt32:
-            case FieldType.Fixed32:
-                return typeof(uint);
-            case FieldType.UInt64:
-            case FieldType.Fixed64:
-                return typeof(ulong);
-            case FieldType.Bool:
-                return typeof(bool);
-            case FieldType.String:
-                return typeof(string);
-            case FieldType.Message when fieldDescriptor.MessageType.ClrType is { } clrType:
-                if (clrType == typeof(DoubleValue))
-                    return typeof(double?);
-                if (clrType == typeof(FloatValue))
-                    return typeof(float?);
-                if (clrType == typeof(Int64Value))
-                    return typeof(long?);
-                if (clrType == typeof(UInt64Value))
-                    return typeof(ulong?);
-                if (clrType == typeof(Int32Value))
-                    return typeof(int?);
-                if (clrType == typeof(UInt32Value))
-                    return typeof(uint?);
-                if (clrType == typeof(BoolValue))
-                    return typeof(bool?);
-                if (clrType == typeof(StringValue))
-                    return typeof(string);
-                return clrType;
-            case FieldType.Enum when fieldDescriptor.IsRepeated:
-                var fieldType = propertyTypeLookup[fieldDescriptor.PropertyName];
-                return fieldType.GenericTypeArguments[0];
-            case FieldType.Enum:
-            case FieldType.Message:
-                return propertyTypeLookup[fieldDescriptor.PropertyName];
-            default:
-                throw new ArgumentOutOfRangeException(nameof(fieldDescriptor), $"FieldType: '{fieldDescriptor}' is not supported.");
-        }
     }
 }
