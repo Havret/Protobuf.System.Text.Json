c8371a0a12fcc989c29e19128e3110cfcef4a7e5
Kristoffer Lerbæk Pedersen <kristp@danskebank.dk>
2023-08-25 12:02:54 +0200

    Add ByteString converter

---
+++ src/Protobuf.System.Text.Json/InternalConverters/ByteStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using Protobuf.System.Text.Json.InternalConverters;
+
+internal class ByteStringConverter : InternalConverter
+{
+    public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+    {
+        var base64String = ((ByteString)value).ToBase64();
+        writer.WriteStringValue(base64String);
+    }
+
+    public override void Read(ref Utf8JsonReader reader, IMessage obj, Type typeToConvert, JsonSerializerOptions options,
+        IFieldAccessor fieldAccessor)
+    {
+        var base64String = reader.GetString();
+        if (base64String is null)
+        {
+            return;
+        }
+        var value = ByteString.FromBase64(base64String);
+        fieldAccessor.SetValue(obj, value);
+    }
+}
